8a621e0d9151a980faf6ee4536573904ed381525
windylung <jisoolee031@gmail.com>
2022-09-06 22:04:57 +0900

    a

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { COLOR_YELLOW } from './color';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>08/19</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+{/* <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer> */}
+
+const Tab = createBottomTabNavigator();
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLOR_YELLOW,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title : {
+    fontSize: 20,
+    fontWeight: "700"
+    
+  }
 });
